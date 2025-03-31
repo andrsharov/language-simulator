259fc6c03ec5cc810ec13cf233a95ee690da78fe
andrsharov <andrsharov@gmail.com>
2025-04-01 00:18:54 +0300

    Fix "django.template.exceptions.TemplateDoesNotExist: index.html" error

--- app/simulator/views.py
+++ app/simulator/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.shortcuts import render
-
 def index(request):
-    context = {
-        'products': ['Товар 1', 'Товар 2', 'Товар 3'],
-    }
-    return render(request, 'simulator/index.html', context)
+    return render(request, "index.html")
+
+
+# def index(request):
+#     context = {
+#         'products': ['Товар 1', 'Товар 2', 'Товар 3'],
+#     }
+#     return render(request, 'simulator/index.html', context)
