be4b9533f2687ecfb35d19a7085097df1db5c101
andrsharov <andrsharov@gmail.com>
2025-04-06 15:57:27 +0300

    Add random card to main page template

--- app/simulator/views.py
+++ app/simulator/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from .forms import CardForm
 from .models import EnglishCard
+import random
 
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html")
+    all_cards = list(EnglishCard.objects.all())
+    random_cards = random.sample(all_cards, min(3, len(all_cards))) if all_cards else []
+    return render(request, "index.html",{'random_cards': random_cards})
 
 def about(request):
     return render(request, "about.html")
@@ -21,7 +24,6 @@ def add_card(request):
         form = CardForm()
 
     return render(request, 'add_card.html', {'form': form})
-
 
 def cards_list(request):
     cards = EnglishCard.objects.all().order_by('-created_at')
